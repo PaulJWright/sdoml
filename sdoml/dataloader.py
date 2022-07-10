e43ea494951d9ae79175502340abe18b733d68b8
Paul Wright <paul@pauljwright.co.uk>
2022-07-10 01:47:39 +0100

    Feature/dataloader: toy DataLoader (#1)

---
+++ sdoml/dataloader.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+
+class SDOMLDataLoader(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
