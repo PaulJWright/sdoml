00467da2e6ed2ddc05dde60383a5c2d56c1f24a8
Paul Wright <paul@pauljwright.co.uk>
2022-07-15 18:27:07 +0100

    changing how version.py is generated to satisfy black

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ VERSION_TEMPLATE = """
 try:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
+
 except Exception:
     __version__ = '{version}'
 """.lstrip()
